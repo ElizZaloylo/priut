d36b292696b5c2a2d8ae328377a39f5f505682b3
Elizaveta <pandacomic22@gmail.com>
2022-05-22 16:17:09 +0400

    Branch count changes

--- src/main/java/com/example/demo/controller/PriutController.java
+++ src/main/java/com/example/demo/controller/PriutController.java
@@ -18,22 +18,22 @@ public class PriutController {
     }
 
     @GetMapping("/{id}/animals/count")
-    private Integer getAnimalsCount(@PathVariable(value="id") Integer id){
+    private Integer getAnimalsCount(@PathVariable(value = "id") Integer id) {
         return priutService.getAnimalsCount(id);
     }
 
     @GetMapping("/{id}/cats/count")
-    private Integer getCatsCount(@PathVariable(value ="id") Integer id){
+    private Integer getCatsCount(@PathVariable(value = "id") Integer id) {
         return priutService.getCatsCount(id);
     }
 
     @GetMapping("/{id}/dogs/count")
-    private Integer getDogsCount(@PathVariable(value="id") Integer id){
+    private Integer getDogsCount(@PathVariable(value = "id") Integer id) {
         return priutService.getDogsCount(id);
     }
 
     @GetMapping("/{id}/birds/count")
-    private Integer getBirdsCount(@PathVariable(value="id") Integer id){
+    private Integer getBirdsCount(@PathVariable(value = "id") Integer id){
         return priutService.getBirdsCount(id);
     }
 
